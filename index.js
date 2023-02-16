2718c4ccdcdfeca93d2ff6e4c0f077811b755ed2
Ryan Garner <rpgarner22@gmail.com>
2023-02-16 16:08:34 -0700

    launch browser setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const { Builder, By} = require("selenium-webdriver");
+
+
+
+async function Tests() {
+//launch Browser
+    let driver = await new Builder().forBrowser("chrome").build()
+
+// navigate to application
+
+//Test to run
+
+//close browser
+
+
+}
+
+Tests()
