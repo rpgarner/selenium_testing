31aa5ddec4aaed6a8703c5491b37d967329ef1b2
Ryan Garner <rpgarner22@gmail.com>
2023-02-16 17:07:42 -0700

    set up safair and chrome

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const { Builder, By} = require("selenium-webdriver");
-
+const {Builder, By} = require('selenium-webdriver');
 
 
 async function Tests() {
 //launch Browser
-    let driver = await new Builder().forBrowser("chrome").build()
-
+   //////////////////// Safari ///////////////////////
+    let driver = await new Builder().forBrowser("safari").build()
+   /////////////un commet below for chrome //////////////// 
+    // const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
 // navigate to application
-
+    await driver.get("https://snap.modernemortgage.com/home/mobile-test");
 //Test to run
-
+await driver.findElement(By.id("teal-job-tracker-root-stable")).isDisplayed();
 //close browser
 
 
