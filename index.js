2d604a228e45b81dfbc5078309c04aed26fb067f
Ryan Garner <rpgarner22@gmail.com>
2023-02-16 17:14:09 -0700

    setting up test 2

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const {Builder, By} = require('selenium-webdriver');
-
+const { Builder, By } = require("selenium-webdriver");
 
 async function Tests() {
-//launch Browser
-   //////////////////// Safari ///////////////////////
-    let driver = await new Builder().forBrowser("safari").build()
-   /////////////un commet below for chrome //////////////// 
-    // const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
-// navigate to application
-    await driver.get("https://snap.modernemortgage.com/home/mobile-test");
-//Test to run
-await driver.findElement(By.id("teal-job-tracker-root-stable")).isDisplayed();
-//close browser
+  //////////////////launch Browser//////////////////
+            ///////// Safari /////////////
+  let driver = await new Builder().forBrowser("safari").build();
+  /////////////uncommet below for chrome ////////////////
+  // const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
+  
+  // navigate to application
+  await driver.get("https://snap.modernemortgage.com/home/mobile-test");
+  //Test to run
+  ////////////////////Test 1 ///////////////////
+  await driver
+    .findElement(
+      By.xpath("/html/body/div[2]/div/div[2]/div/div/div[3]/div[3]/span[2]/a")
+    )
+    .isDisplayed();
 
-
+    ////////////////////Test 2////////////////////
+  //close browser
 }
 
-Tests()
+Tests();
